78a5172b089989af5e964a0979acd73ff127b7ab
Johnny897 <job.cad@ukr.net>
2017-01-12 10:07:25 +0200

    Temporary files removing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,14 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(ui->login->text() == "user7" && ui->password->text() == "8976") {
+        QMessageBox::information(this, "Password confirmed", "Your password is proper");
+    }
+    else {
+        QMessageBox::warning(this, "Wrong password", "Your password is wrong!!!");
+    }
 }
